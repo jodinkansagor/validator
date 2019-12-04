7ac37d000c2a3eae35b6bed69515f795b1891f8d
Jodi N Kansagor <joedvice@gmail.com>
2019-12-03 16:37:30 -0800

    about to test, wrote schema

---
+++ __tests__/schema.test.js
@@ -0,0 +1,46 @@
+const { Validator } = require('../lib/validator');
+const { Schema } = require('../lib/schema');
+
+const schema = new Schema({
+    name: {
+        type: String,
+        required: true
+    }, 
+    age: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: String
+    }
+});
+  
+const spot = {
+    name: 'spot',
+    age: 5,
+    weight: '20 lbs'
+};
+  
+const rover = {
+    name: 'rover',
+    age: '10'
+};
+  
+const who = {
+    age: 'hi'
+};
+
+describe('schema module', () => {
+
+    //1
+    it('returns an object with all the right fields cast', () => {
+        expect(schema.validate(spot)).toEqual({ name: 'spot', age: 5, weight: '20 lbs' });
+        expect(schema.validate(rover)).toEqual({ name: 'rover', age: 10 });
+    });
+
+    //2
+    it('throws an error if the object doesnt pass the schema', () => {
+        expect(() => schema.validate(who)).toThrowErrorMatchingSnapshot();
+    });
+
+});
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { isNumber, isString, isBoolean, isArray, isObject, isFunction } = require('./lib/types.js');
 const  { Validator } = require('./lib/validator.js');
 
+const { Schema } = require('./lib/schema.js');
+
 const dog = {
     name: 'spot',
     age: '5',
@@ -12,10 +14,30 @@ const nameValidator = new Validator('name', {
     required: true
 });
 
+const schema = new Schema({
+    name: {
+        type: String,
+        required: true
+    }, 
+    age: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: String
+    }
+});
+
+const spot = {
+    name: 'spot',
+    age: 5,
+    weight: '20 lbs'
+};
 // console.log(isNumber('3'));
 // console.log(isString('hi'));
 // console.log(isBoolean(true));
 // console.log(isArray([3, 2]));
 // console.log(isObject({ name: 'JBJ', age: 41 }));
 // console.log(isFunction(() => {}));
-console.log(nameValidator.validate(dog));
+// console.log(nameValidator.validate(dog));
+console.log(schema);
